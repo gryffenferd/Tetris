79a8d155f99eb9bac85e4d2c53739ee0f7ee5f0c
gryffenferd <axeljacki@gmail.com>
2016-01-07 15:08:56 +0100

    Changement de l'envoi du server vers client

--- src/servernio/EchoServer.java
+++ src/servernio/EchoServer.java
@@ -161,16 +161,14 @@ public class EchoServer {
 			if(id == 0){
 				if(counterJ2toServer > counterServertoJ1){
 					System.out.println(id + " : " + cmdJ2.get(counterServertoJ2));
-					counterServertoJ1++;
-					this.writeMessage(socket, "commande:" + cmdJ2.get(counterServertoJ2++));
+					this.writeMessage(socket, "commande:" + cmdJ2.get(counterServertoJ1++));
 				}else{
 					this.writeMessage(socket, "commande:0");
 				}
 			}else{
 				if(counterJ1toServer > counterServertoJ2){
 					System.out.println(id + " : " + cmdJ1.get(counterServertoJ1));
-					counterServertoJ2++;
-					this.writeMessage(socket, "commande:" + cmdJ1.get(counterServertoJ1++));
+					this.writeMessage(socket, "commande:" + cmdJ1.get(counterServertoJ2++));
 				}else{
 					this.writeMessage(socket, "commande:0");
 				}
