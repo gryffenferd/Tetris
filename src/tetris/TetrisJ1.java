af10b145f42d77ca25e4745e5ca7989e36df1b7f
Cyprien <Cyprien@10.3.4.210>
2016-01-07 14:37:15 +0100

    essai de focus sur la fenetre

--- src/tetris/TetrisJ1.java
+++ src/tetris/TetrisJ1.java
@@ -661,14 +661,14 @@ public class TetrisJ1 extends Applet {
 		
 		// add the key listener to all components that might get focus
 		// so that it'll work regardless of which has focus
-		start_newgame_butt.addKeyListener(key_listener);
+		this.addKeyListener(key_listener);
 		
 		Panel right_panel = new Panel(new GridLayout(3, 1));	
 		right_panel.setBackground(BACKGROUND_COLOR);
 		
 		Panel control_panel = new Panel();
 		control_panel.add(start_newgame_butt);
-		start_newgame_butt.setFocusable(true);
+		//start_newgame_butt.setFocusable(true);
 		control_panel.setBackground(BACKGROUND_COLOR);
 		right_panel.add(control_panel);
 
@@ -688,7 +688,7 @@ public class TetrisJ1 extends Applet {
 		right_panel.add(tmp);
 		
 		// finaly, add all the main panels to the applet panel
-		start_newgame_butt.setFocusable(true);
+		//start_newgame_butt.setFocusable(true);
 		this.setLayout(new GridLayout(1, 2));
 		this.add(game_grid);
 		this.add(right_panel);
